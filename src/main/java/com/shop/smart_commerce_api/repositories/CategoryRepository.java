9da76aa6f8d2d81cea2e58e1763e06ba4dee6639
Phuong <thephuongop12@gmail.com>
2025-05-30 22:45:39 +0700

    save

--- src/main/java/com/shop/smart_commerce_api/repositories/CategoryRepository.java
+++ src/main/java/com/shop/smart_commerce_api/repositories/CategoryRepository.java
@@ -1,5 +1,11 @@
 package com.shop.smart_commerce_api.repositories;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shop.smart_commerce_api.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
