0aa43febaa2cf81a8bbea4dd122200613e1fed29
Phuong <thephuongop12@gmail.com>
2025-07-10 01:22:24 +0700

    change and reset password

--- src/main/java/com/shop/smart_commerce_api/services/RedisService.java
+++ src/main/java/com/shop/smart_commerce_api/services/RedisService.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import com.shop.smart_commerce_api.model.ForgotPassword;
 import com.shop.smart_commerce_api.model.OtpSession;
 
 import jakarta.annotation.PostConstruct;
@@ -18,8 +19,12 @@ public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
     private final RedisTemplate<String, Object> objectRedisTemplate;
 
-    public void saveObject(String key, OtpSession otpSession, long tllMinutes) {
-        objectRedisTemplate.opsForValue().set(key, otpSession, Duration.ofMinutes(tllMinutes));
+    public void saveObject(String key, Object object, long ttlMinutes) {
+        objectRedisTemplate.opsForValue().set(key, object, Duration.ofMinutes(ttlMinutes));
+    }
+
+    public void deleteObject(String key) {
+        objectRedisTemplate.delete(key);
     }
 
     public Object getObject(String key) {
