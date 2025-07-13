17cdfc2022550ffc8caabea9d6b3bbaf0252750d
Phuong <thephuongop12@gmail.com>
2025-07-13 16:13:12 +0700

    add controller update profile

--- src/main/java/com/shop/smart_commerce_api/controllers/MeController.java
+++ src/main/java/com/shop/smart_commerce_api/controllers/MeController.java
@@ -1,15 +1,12 @@
 package com.shop.smart_commerce_api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.shop.smart_commerce_api.dto.response.ApiResponse;
 import com.shop.smart_commerce_api.dto.response.user.UserResponse;
 import com.shop.smart_commerce_api.services.UserService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +16,15 @@ public class MeController {
 
     @GetMapping("/profile")
     public ApiResponse<UserResponse> getProfile() {
-        return ApiResponse.<UserResponse>builder()  
+        return ApiResponse.<UserResponse>builder()
                 .code(200)
                 .message("Get profile successfully")
                 .data(userService.getCurrentProfile())
                 .build();
     }
 
+    @PostMapping("/profile/update")
+    public ApiResponse<UserResponse> updateProfile() {
+        return ApiResponse.<UserResponse>builder().code(200).data(null).build();
+    }
 }
---
+++ src/main/java/com/shop/smart_commerce_api/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.shop.smart_commerce_api.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+}
