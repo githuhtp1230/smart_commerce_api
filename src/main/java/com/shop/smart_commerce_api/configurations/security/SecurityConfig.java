c7671aab6132971a244be51a03e6a6d599f5cbc0
thanhdppd09926 <thanhdppd09926@gmail.com>
2025-08-16 13:33:04 +0700

    get permission

--- src/main/java/com/shop/smart_commerce_api/configurations/security/SecurityConfig.java
+++ src/main/java/com/shop/smart_commerce_api/configurations/security/SecurityConfig.java
@@ -61,9 +61,9 @@ public class SecurityConfig {
             "api/me/order-details/**",
             "api/permissions",
             "api/permissions/**",
-            "api/role-permissions/**",
             "api/role-permissions",
-            "api/roles/*/permissions/**"
+            "api/roles/*/permissions/**",
+            "api/role-permissions/**"
     };
 
     @Bean
